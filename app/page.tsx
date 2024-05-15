22844ebae598426993138dd4cd59c1f3fcc12f2c
paulmwatson <github.com@paulmwatson.com>
2024-05-15 18:47:07 +0200

    Basic name filter

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import { PowerStation } from "@/types";
 import Map from "@/components/map";
 import { useEffect, useState } from "react";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
 async function getData() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/power-stations`);
@@ -10,18 +11,39 @@ async function getData() {
 }
 
 export default function Page() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const currentSearchParams = useSearchParams();
   const [powerStations, setPowerStations] = useState<PowerStation[]>([]);
+  const [filterName, setFilterName] = useState(
+    currentSearchParams.get("name") ?? ""
+  );
+  const filteredPowerStations = powerStations.filter(({ name }) =>
+    name.toLowerCase().includes(filterName.toLowerCase())
+  );
   useEffect(() => {
     getData().then(setPowerStations);
   }, []);
+
+  const updatedSearchParams = new URLSearchParams(
+    currentSearchParams.toString()
+  );
+
+  const changeFilterName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterName(e.target.value);
+    updatedSearchParams.set("name", e.target.value);
+    router.push(pathname + "?" + updatedSearchParams.toString());
+  };
   return (
     <main>
-      <div className="mapContainer"
-        
-      >
-        <Map powerStations={powerStations} />
+      <div className="mapContainer">
+        <Map powerStations={filteredPowerStations} />
       </div>
       <aside>
+        <label>
+          Filter Name:
+          <input type="text" value={filterName} onChange={changeFilterName} />
+        </label>
         <table>
           <thead>
             <tr>
@@ -31,7 +53,7 @@ export default function Page() {
             </tr>
           </thead>
           <tbody>
-            {powerStations.map(({ name, position }) => (
+            {filteredPowerStations.map(({ name, position }) => (
               <tr key={name}>
                 <td>{name}</td>
                 <td>{position.lat}</td>
