d0aa85deebc5676c055326acd2f1e31763b05f28
paulmwatson <github.com@paulmwatson.com>
2024-05-29 22:08:09 +0200

    Add more info window on click

--- components/powerStationMarker/index.tsx
+++ components/powerStationMarker/index.tsx
@@ -2,7 +2,10 @@ import "./index.scss";
 import { Marker, InfoWindow, useMarkerRef } from "@vis.gl/react-google-maps";
 import { PowerStation } from "@/types";
 import { useState } from "react";
-import { Chip } from "@mui/material";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 type Props = {
   powerStation: PowerStation;
@@ -11,14 +14,26 @@ type Props = {
 export function PowerStationMarker({ powerStation }: Props) {
   const [markerRef, marker] = useMarkerRef();
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [isHoverOpen, setHoverIsOpen] = useState(false);
+  const [isMoreOpen, setMoreIsOpen] = useState(false);
 
-  const showInfoWindow = () => {
-    setIsOpen(true);
+  const showHoverInfoWindow = () => {
+    if (!isMoreOpen) {
+      setHoverIsOpen(true);
+    }
   };
 
-  const hideInfoWindow = () => {
-    setIsOpen(false);
+  const hideHoverInfoWindow = () => {
+    setHoverIsOpen(false);
+  };
+
+  const showMoreInfoWindow = () => {
+    hideHoverInfoWindow();
+    setMoreIsOpen(true);
+  };
+
+  const hideMoreInfoWindow = () => {
+    setMoreIsOpen(false);
   };
 
   return (
@@ -27,17 +42,20 @@ export function PowerStationMarker({ powerStation }: Props) {
         ref={markerRef}
         position={powerStation.position}
         title={powerStation.name}
-        onMouseOver={showInfoWindow}
-        onMouseOut={hideInfoWindow}
+        onMouseOver={showHoverInfoWindow}
+        onMouseOut={hideHoverInfoWindow}
+        onClick={showMoreInfoWindow}
       />
-      {isOpen && (
+      {isHoverOpen && (
         <InfoWindow
           anchor={marker}
-          onCloseClick={hideInfoWindow}
+          onCloseClick={hideHoverInfoWindow}
           className="powerStationHoverInfo"
         >
           <div>
-            <h1>{powerStation.name}</h1>
+            <Typography variant="h1" component="h1">
+              {powerStation.name}
+            </Typography>
             <p>
               <Chip
                 label={powerStation.fuelType.name}
@@ -47,6 +65,88 @@ export function PowerStationMarker({ powerStation }: Props) {
               {powerStation.powerOutput} MW
             </p>
             <p>{powerStation.operator?.name}</p>
+          </div>
+        </InfoWindow>
+      )}
+      {isMoreOpen && (
+        <InfoWindow
+          anchor={marker}
+          onCloseClick={hideMoreInfoWindow}
+          className="powerStationMoreInfo"
+        >
+          <div>
+            <Stack alignItems="center" direction="row" gap={2}>
+              <Typography variant="h1" component="h1">
+                {powerStation.name}
+              </Typography>
+              <Button
+                size="small"
+                onClick={hideMoreInfoWindow}
+                sx={{ marginLeft: "auto" }}
+              >
+                X
+              </Button>
+            </Stack>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Type:</td>
+                  <td>
+                    <Chip
+                      label={powerStation.fuelType.name}
+                      variant="filled"
+                      size="small"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Location:</td>
+                  <td>{powerStation.region.name}</td>
+                </tr>
+                <tr>
+                  <td>GPS:</td>
+                  <td>
+                    {powerStation.position.lat}, {powerStation.position.lng}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Operator:</td>
+                  <td>{powerStation.operator?.name}</td>
+                </tr>
+                <tr>
+                  <td>Power Output:</td>
+                  <td>{powerStation.powerOutput} MW</td>
+                </tr>
+                {powerStation.age.commissionStart &&
+                  powerStation.age.commissionEnd && (
+                    <tr>
+                      <td>Commissioned:</td>
+                      <td>
+                        {new Date(
+                          powerStation.age.commissionStart
+                        ).getFullYear()}{" "}
+                        -
+                        {new Date(powerStation.age.commissionEnd).getFullYear()}
+                      </td>
+                    </tr>
+                  )}
+                {powerStation.age.decommissionStart &&
+                  powerStation.age.decommissionEnd && (
+                    <tr>
+                      <td>Decommissioned:</td>
+                      <td>
+                        {new Date(
+                          powerStation.age.decommissionStart
+                        ).getFullYear()}{" "}
+                        -
+                        {new Date(
+                          powerStation.age.decommissionEnd
+                        ).getFullYear()}
+                      </td>
+                    </tr>
+                  )}
+              </tbody>
+            </table>
           </div>
         </InfoWindow>
       )}
